37a1b48019c21527aa65fdb2f434f71587b7d9f5
fatma1959 <hamdif31@gmail.com>
2021-07-26 14:45:58 +0100

    firstcommit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Address = () => {
+    return (
+        <div>
+            <p> Cité Bahri 3</p>
+        </div>
+    )
+}
+
+export default Address
+
+
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img className ="image1" src='/Wb.jpg' alt=""></img>
+        </div>
+    )
+}
+
+export default ProfilePhoto
